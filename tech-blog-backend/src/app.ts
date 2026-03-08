96d529aa23c6d542e6ff298725160d0585ec693e
KeaMeng <2023484ing@aupp.edu.kh>
2026-03-08 14:26:35 +0700

    User Routes and Controller

--- tech-blog-backend/src/app.ts
+++ tech-blog-backend/src/app.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import authRouter from "./routes/auth.routes.js";
 import postRouter from "./routes/post.routes.js";
 import categoryRouter from "./routes/category.routes.js";
+import userRouter from "./routes/user.routes.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/categories", categoryRouter);
+app.use("/api/users", userRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
