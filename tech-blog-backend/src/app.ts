06a0e5edfc59fd0e691872a7a59816600f942ddf
keameng <2023484ing@aupp.edu.kh>
2026-05-16 23:11:27 +0700

    Deploy Config

--- tech-blog-backend/src/app.ts
+++ tech-blog-backend/src/app.ts
@@ -14,9 +14,8 @@ import { startNewsFetcherJob } from "./jobs/newsFetcher.job.js";
 const app = express();
 const port = process.env.PORT || 5000;
 
-//app.use(cors());
 app.use(cors({
-  origin: "http://localhost:3001",
+  origin: process.env.FRONTEND_URL || "http://localhost:3001",
   credentials: true
 }));
 app.use(express.json());
